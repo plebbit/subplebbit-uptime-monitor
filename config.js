4a8b6928ad5604f741a71c77af019016c5f55736
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-04-03 00:07:04 +0000

    fix ens bug

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
 module.exports = {
+  chainProviders: {
+    eth: {
+      // if ETH RPC URL, won't use default ethers provider
+      url: process.env.ETH_PROVIDER_URL
+    }
+  },
   ipfs: {
     // if gateway URL is defined, monitor won't start its own IPFS daemon
-    gatewayUrl: ''
+    gatewayUrl: 'https://ipfs.io'
   },
   monitor: {
     interval: 1000 * 60 * 10 // 10 minutes
