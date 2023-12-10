5473fde193dbbf42aada25b4339ef39f30a20e5b
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-12-10 02:43:55 +0000

    fix deploy script to use new default multisub

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,6 +16,8 @@ if [ -z "${DEPLOY_HOST+xxx}" ]; then echo "DEPLOY_HOST not set" && exit; fi
 if [ -z "${DEPLOY_USER+xxx}" ]; then echo "DEPLOY_USER not set" && exit; fi
 if [ -z "${DEPLOY_PASSWORD+xxx}" ]; then echo "DEPLOY_PASSWORD not set" && exit; fi
 
+SUBPLEBBITS=$(node -e "fetch('https://raw.githubusercontent.com/plebbit/temporary-default-subplebbits/master/multisub.json ').then(res => res.json()).then(multisub => console.log(multisub.subplebbits.map(subplebbit => subplebbit.address).join('\n')))")
+
 SCRIPT="
 cd /home
 git clone https://github.com/plebbit/subplebbit-uptime-monitor.git
@@ -25,7 +27,7 @@ git pull
 npm install
 
 # download subs from github
-curl https://raw.githubusercontent.com/plebbit/temporary-default-subplebbits/master/subplebbits.json | grep -Po '(?<=\"address\": \")[^\"]+' > subplebbits.txt
+echo '$SUBPLEBBITS' > subplebbits.txt
 "
 
 # execute script over ssh
